1a020661b9f3212704ea2cea93396fb2158ac5fe
andhedrew <theandhedrew@gmail.com>
2023-03-31 14:46:42 -0500

    dirt now hurts player properly

--- dirt_block.gd
+++ dirt_block.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
+@onready var hit_box := $Hitbox
 
 func _ready():
 	$Hurtbox.area_entered.connect(_on_area_entered)
-	$Hitbox.monitorable = false
 
 func _physics_process(delta):
+	
+	velocity.y += Param.GRAVITY*.02
+	var colliding = move_and_collide(velocity*delta)
+	if colliding:
+		velocity.y = 0
+		hit_box.monitorable = false
+		hit_box.position.y = -32
+	else:
+		hit_box.monitorable = true
+		hit_box.position.y = lerp(hit_box.position.y, -16.0, .5)
 
-	move_and_slide()
-	if !is_on_floor():
-		velocity.y += Param.GRAVITY * delta
-		$Hitbox.monitorable = true
-		velocity.y += Param.GRAVITY * delta
-	else: 
-		$Hitbox.monitorable = false
 
 func _on_area_entered(hitbox) -> void:
 	if hitbox is HitBox:
