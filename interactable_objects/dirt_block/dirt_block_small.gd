79a97ef87d8d192533c0bf1df28e52877b098bde
andhedrew <theandhedrew@gmail.com>
2023-03-31 15:03:46 -0500

    small dirt blocks

---
+++ interactable_objects/dirt_block/dirt_block_small.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+@onready var hit_box := $Hitbox
+
+func _ready():
+	$Hurtbox.area_entered.connect(_on_area_entered)
+
+func _physics_process(delta):
+	
+	velocity.y += Param.GRAVITY*.02
+	var colliding = move_and_collide(velocity*delta)
+	if !colliding:
+		if $PlayerEnemyDetector.is_colliding() or $PlayerEnemyDetector2.is_colliding() or $PlayerEnemyDetector3.is_colliding():
+			$CollisionShape2D.queue_free()
+			_destroy()
+	else:
+		velocity.y = 0
+	
+	
+
+
+func _on_area_entered(hitbox) -> void:
+	if hitbox is HitBox:
+		if !hitbox.fire and !hitbox.syphon:
+			var dirt := preload("res://vfx/dirt_explode.tscn").instantiate()
+			dirt.restart()
+			dirt.position = global_position
+			get_node("/root/").add_child(dirt)
+			queue_free()
+
+
+func _destroy() -> void:
+	var dirt := preload("res://vfx/dirt_explode.tscn").instantiate()
+	dirt.restart()
+	dirt.amount = 15
+	dirt.position = global_position
+	get_node("/root/").add_child(dirt)
+	queue_free()
