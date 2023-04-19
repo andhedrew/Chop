a8a1f34d658ea4bdb7d8bf6d03e2fb01f75da78d
andhedrew <theandhedrew@gmail.com>
2023-04-19 14:43:19 -0500

    Food bars added to little brother

--- characters/little_brother/GenerateCollectedFood.gd
+++ characters/little_brother/GenerateCollectedFood.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
 @onready var animation_player := $"../Pivot/AnimationPlayer"
+@onready var brick_hunger_bar := $"../BrickHungerBar"
+@onready var plant_hunger_bar := $"../PlantHungerBar"
+@onready var meat_hunger_bar := $"../MeatHungerBar"
 var food_collected := 0
-@onready var collision_polygon := $"../CollisionPolygon2D"
+#@onready var collision_polygon := $"../CollisionPolygon2D"
 
 func _ready():
 	self.body_entered.connect(_generate_food)
@@ -13,7 +16,7 @@ func _generate_food(player) -> void:
 	if player.bag.size() > 0:
 		GameEvents.cutscene_started.emit()
 		player.facing = Enums.Facing.LEFT
-		get_node("../CollisionPolygon2D").set_deferred("disabled", true)
+#		get_node("../CollisionPolygon2D").set_deferred("disabled", true)
 		animation_player.play("eat")
 		for item in player.bag:
 				GameEvents.removed_food_from_bag.emit()
@@ -24,13 +27,16 @@ func _generate_food(player) -> void:
 				pickup.position = $FoodSpawn.global_position
 				pickup.velocity = Vector2(0, randf_range(-4, -6))
 				food_collected += 1
+				brick_hunger_bar.value += pickup.brick_value
+				plant_hunger_bar.value += pickup.plant_value
+				meat_hunger_bar.value += pickup.meat_value
 				await get_tree().create_timer(0.3).timeout
 				SoundPlayer.play_sound("glottal_stop")
 		player.bag = []
 		await get_tree().create_timer(1.0).timeout
 		if food_collected > 0:
 			SoundPlayer.play_sound("chew")
-			animation_player.play("chew")
+#			animation_player.play("chew")
 			_choose_emotion()
 		else:
 			animation_player.play("idle")
@@ -39,23 +45,25 @@ func _generate_food(player) -> void:
 func _choose_emotion() -> void:
 	if food_collected > 3:
 		SoundPlayer.play_sound("little_brother_happy")
-		_play_happy()
+#		_play_happy()
+		animation_player.play("idle")
 		food_collected = 0
 	else:
 		SoundPlayer.play_sound("little_brother_sad")
-		_play_sad()
+#		_play_sad()
+		animation_player.play("idle")
 		food_collected = 0
-	get_node("../CollisionPolygon2D").set_deferred("disabled", false)
+#	get_node("../CollisionPolygon2D").set_deferred("disabled", false)
 	
 	GameEvents.cutscene_ended.emit()
 
 
-func _play_happy():
-	await get_tree().create_timer(1.5).timeout
-	animation_player.play("happy")
-
-
-
-func _play_sad():
-	await get_tree().create_timer(1.7).timeout
-	animation_player.play("sad")
+#func _play_happy():
+#	await get_tree().create_timer(1.5).timeout
+#	animation_player.play("happy")
+#
+#
+#
+#func _play_sad():
+#	await get_tree().create_timer(1.7).timeout
+#	animation_player.play("sad")
--- pickups/food_pickup.gd
+++ pickups/food_pickup.gd
@@ -1,8 +1,32 @@
 extends Pickup
 
+var brick_value :int = 0
+var meat_value :int = 0
+var plant_value :int = 0
+
+var nutrition_value_lookup : Dictionary = {
+	load("res://characters/enemy/default/mallow_pieces1.png") : {"brick" : 0, "meat" : 2, "plant" : 0 },
+	load("res://characters/enemy/default/mallow_pieces2.png") : {"brick" : 3, "meat" : 0, "plant" : 0 },
+	load("res://characters/enemy/default/mallow_pieces3.png") : {"brick" : 0, "meat" : 0, "plant" : 4 },
+}
+
 
 func _add_pickup_to_inventory(player) -> void:
 	if player.bag_capacity > player.bag.size():
 		player.bag.append($Sprite2D.texture)
 		GameEvents.added_food_to_bag.emit($Sprite2D.texture)
 		_destroy(player.position)
+
+
+func setup(new_texture: CompressedTexture2D) -> void:
+	$Sprite2D.texture = new_texture
+	var size = $Sprite2D.texture.get_size()
+	$CollisionShape2D.shape = RectangleShape2D.new()
+	$CollisionShape2D.shape.extents = size / 2
+	
+	brick_value = nutrition_value_lookup[new_texture]["brick"]
+	meat_value = nutrition_value_lookup[new_texture]["meat"]
+	plant_value = nutrition_value_lookup[new_texture]["plant"]
+	print_debug("brick" + str(brick_value))
+	print_debug("meat" + str(meat_value))
+	print_debug("plant" + str(plant_value))
