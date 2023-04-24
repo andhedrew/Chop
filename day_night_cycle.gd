9dfc67ba8272467cf6a0f013f6abaabbd2b2372b
andhedrew <theandhedrew@gmail.com>
2023-04-24 11:58:37 -0500

    sunrise, sunset

--- day_night_cycle.gd
+++ day_night_cycle.gd
@@ -3,7 +3,7 @@ extends AnimationPlayer
 
 func _process(delta):
 	if Input.is_action_just_pressed("1"):
-		play("day")
+		play("sunrise")
 	if Input.is_action_just_pressed("2"):
 		play("day")
 	if Input.is_action_just_pressed("3"):
