c2874410e191c2be7bd340623a473b3fcf751587
andhedrew <theandhedrew@gmail.com>
2023-05-16 13:54:27 -0500

    Blower directional

--- autoloads/SortLayer.gd
+++ autoloads/SortLayer.gd
@@ -1,9 +1,10 @@
 extends Node
 
 const BACKGROUND = -3
+const PROPS = -2
 const FOREGROUND = -1
-const PLAYER = 0
+const PLAYER = 0 # Tilemap is at this depth
 const IN_FRONT = 1
 const HUD = 2
 const EFFECTS = -1
-const PROPS = -2
+
--- levels_and_level_objects/level_scenes/city/blower.gd
+++ levels_and_level_objects/level_scenes/city/blower.gd
@@ -1,26 +1,38 @@
 extends Area2D
 
-# NEEDS: directions
-
-
-
-# Set the upward force to apply
-@export var upward_force = Vector2(0, -10)
+@export var force = Vector2(10, -10)
+@export_enum("UP", "LEFT", "DOWN", "RIGHT") var blow_direction: String
 var player_in_area := false
 var player = null
+@onready var particles: GPUParticles2D = $GPUParticles2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
 func _ready():
 	self.body_entered.connect(_on_body_entered)
 	self.body_exited.connect(_on_body_exited)
-	var visibility_rect = $GPUParticles2D.visibility_rect
-	visibility_rect.size = $CollisionShape2D.shape.extents * 2
 
+	z_index = SortLayer.FOREGROUND
+	
+	var collision_width = collision_shape.shape.extents.x
+	var collision_height = collision_shape.shape.extents.y
+
+	
 
 func _physics_process(delta):
 	if player_in_area:
-	
-		player.velocity += upward_force
-		player.velocity.y = clamp(player.velocity.y, -250, 0)
-		print_debug(str(player.velocity.y))
+		match blow_direction:
+			"UP":
+				player.velocity.y += force.y
+				player.velocity.y = clamp(player.velocity.y, -250, 0)
+			"LEFT":
+				player.velocity.x -= force.x
+				player.velocity.y = clamp(player.velocity.y, 250, 0)
+			"DOWN":
+				player.velocity.y -= force.y
+				player.velocity.x = clamp(player.velocity.x, 250, 0)
+			"RIGHT":
+				player.velocity.x += force.x
+				player.velocity.x = clamp(player.velocity.x, -250, 0)
 	else:
 		pass
 
@@ -28,7 +40,6 @@ func _on_body_entered(body):
 	if body is Player:
 		player_in_area = true
 		player = body
-
 
 func _on_body_exited(body):
 	if body is Player:
