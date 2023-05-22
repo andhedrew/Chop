cc37d62d799ec49ed07062ce8f7199093e40531e
andhedrew <theandhedrew@gmail.com>
2023-05-22 14:04:46 -0500

    Store item function now selectable

--- user_interface/store/store_item.gd
+++ user_interface/store/store_item.gd
@@ -1,15 +1,17 @@
 extends Control
 
-
 @export var cost: int = 20
 @export var img: Texture
 @export var description: String
+@export_enum("buy_health", "upgrade_bag_size", "buy_booster", "upgrade_booster_charges") var buy_function: String
+
+var is_active := true
 
 func _ready():
 	$Button.button_down.connect(_on_button_pressed)
 	$ColorRect/MarginContainer/ColorRect/MarginContainer/Panel/MarginContainer/Label.text = description
 	$ColorRect/MarginContainer/ColorRect/MarginContainer/Panel/TextureRect.texture = img
-	$Button.text = str("BUY " + str(cost))
+	$Button.text = str("BUY: " + str(cost))
 
 func _on_button_pressed() -> void:
 	var current_cash = SaveManager.load_item("money")
@@ -20,11 +22,20 @@ func _on_button_pressed() -> void:
 	else:
 		_cannot_buy()
 
-
 func _buy_item() -> void:
-	print_debug("bought_it")
-	pass
+	call(buy_function)
 
 func _cannot_buy() -> void:
 	$AnimationPlayer.play("cant_buy")
 
+func buy_health() -> void:
+	print_debug("health bouthgaeg")
+
+func upgrade_bag_size() -> void:
+	print_debug("hupgrab bbough")
+
+func buy_booster() -> void:
+	pass
+
+func upgrade_booster_charges() -> void:
+	pass
