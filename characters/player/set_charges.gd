c35e0b160930b4de48818b7b285d5a4093f6ace3
andhedrew <theandhedrew@gmail.com>
2023-05-23 18:05:30 -0500

    Charges are in a circle

--- characters/player/set_charges.gd
+++ characters/player/set_charges.gd
@@ -1,32 +1,3 @@
-#extends Marker2D
-#var sprites = []
-#
-#func _ready() -> void:
-#	_on_charge_amount_changed(owner.torch_charges, owner.max_torch_charges)
-#	GameEvents.charge_amount_changed.connect(_on_charge_amount_changed)
-#
-#
-#func _on_charge_amount_changed(torch_charges, max_torch_charges):
-#	# Remove any existing sprites
-#	for sprite in sprites:
-#		sprite.queue_free()
-#	sprites.clear()
-#
-#	# Create new sprites
-#	for i in range(max_torch_charges):
-#		var sprite = Sprite2D.new()
-#		if i < torch_charges:
-#			sprite.frame = 0
-#		else:
-#			sprite.frame = 1
-#
-#		sprite.texture = preload("res://user_interface/torch_charges/charge.png")
-#		sprite.hframes = 2
-#		sprite.position = Vector2(i * sprite.texture.get_width(), 0)
-#		add_child(sprite)
-#		sprites.append(sprite)
-
-
 extends Marker2D
 
 var sprites = []
@@ -35,7 +6,6 @@ var sprite_width
 func _ready() -> void:
 	_on_charge_amount_changed(owner.torch_charges, owner.max_torch_charges)
 	GameEvents.charge_amount_changed.connect(_on_charge_amount_changed)
-
 
 func _on_charge_amount_changed(torch_charges, max_torch_charges):
 	# Remove any existing sprites
@@ -60,8 +30,51 @@ func _on_charge_amount_changed(torch_charges, max_torch_charges):
 
 	# Center the sprites horizontally
 	var x_offset = (sprite_width - total_width) / 2 +4
+	
+	var radius = 30 # Change this value to adjust the radius of the circle.
+	var angle_step = 360.0 / max_torch_charges
+	
+	var center_x = x_offset + total_width / 2.0
+	
 	for i in range(sprites.size()):
 		var sprite = sprites[i]
-		sprite.position = Vector2(i * sprite.texture.get_width() + x_offset, 0)
+		
+		var angle_degrees = (i * angle_step) - 90.0
+		var angle_radians = angle_degrees * PI / 180.0
+		
+		var x_pos = cos(angle_radians) * radius + center_x
+		var y_pos = sin(angle_radians) * radius
+		
+		sprite.position = Vector2(x_pos, y_pos)
 		
 		add_child(sprite)
+
+
+#func _on_charge_amount_changed(torch_charges, max_torch_charges):
+#	# Remove any existing sprites
+#	for sprite in sprites:
+#		sprite.queue_free()
+#	sprites.clear()
+#
+#	# Create new sprites
+#	var total_width = 0
+#	for i in range(max_torch_charges):
+#		var sprite = Sprite2D.new()
+#
+#		sprite.texture = preload("res://user_interface/torch_charges/charge.png")
+#		sprite.hframes = 2
+#		if i < torch_charges:
+#			sprite.frame = 0
+#		else:
+#			sprite.frame = 1
+#		total_width += sprite.texture.get_width()
+#		sprites.append(sprite)
+#		sprite_width = sprite.texture.get_width()
+#
+#	# Center the sprites horizontally
+#	var x_offset = (sprite_width - total_width) / 2 +4
+#	for i in range(sprites.size()):
+#		var sprite = sprites[i]
+#		sprite.position = Vector2(i * sprite.texture.get_width() + x_offset, 0)
+#
+#		add_child(sprite)
