ef7a843de5375fe2b5d9ee4586d96059c6294fd0
andhedrew <theandhedrew@gmail.com>
2023-09-21 22:55:18 -0500

    ghost tiles not fixed yet

--- sprites/tilesets/breakable_tileset_behavior.gd
+++ sprites/tilesets/breakable_tileset_behavior.gd
@@ -6,12 +6,15 @@ var slice_scene = preload("res://vfx/slice.tscn")
 @export var particle_texture_2: Texture
 
 var tiles_to_return = []
+var return_times: Array = []
 
+
+func _ready():
+		$Timer.timeout.connect(_on_timer_timeout)
+	
 func _destroy_tile(global_pos: Vector2) -> void:
 	var cell_id = get_cell_source_id(0, global_pos)
-	print_debug(str(cell_id))
 	if cell_id == 2:
-		print_debug("replacing tilee")
 		_replace_tile(global_pos)
 	elif cell_id == 1:
 		
@@ -38,28 +41,48 @@ func _destroy_tile(global_pos: Vector2) -> void:
 			explode.process_material.emission_shape = ParticleProcessMaterial.EMISSION_SHAPE_BOX
 			explode.process_material.emission_box_extents = Vector3(cell_size, cell_size, 0)
 
-
 func _replace_tile(global_pos: Vector2) -> void:
-	print_debug("replacing_tiles")
 	var cell_pos = local_to_map(global_pos)
 	var atlas_coords = get_cell_atlas_coords(0, global_pos)
 	set_cell(0, global_pos, 3, atlas_coords)
 	tiles_to_return.append(global_pos)
-	
-	# Start a timer for 2 seconds
-	var timer = Timer.new()
-	timer.set_wait_time(2.0)
-	timer.timeout.connect(_on_timer_timeout)
-	add_child(timer)
-	timer.start()
-	
+	return_times.append(Time.get_unix_time_from_system() + 2)
 
+	$Timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	var current_time = Time.get_unix_time_from_system()
+	while return_times.size() > 0 and return_times[0] <= current_time:
+		_return_tile(tiles_to_return.pop_front())
+		return_times.pop_front()
+	
+	if return_times.size() == 0:
+		$Timer.stop()  # Stop the timer if there are no more tiles to return
+
+	if return_times.size() == 0:
+		$Timer.stop()  # Stop the timer if there are no more tiles to return
+
+#func _replace_tile(global_pos: Vector2) -> void:
+#	var cell_pos = local_to_map(global_pos)
+#	var atlas_coords = get_cell_atlas_coords(0, global_pos)
+#	set_cell(0, global_pos, 3, atlas_coords)
+#	tiles_to_return.append(global_pos)
+#
+#	# Start a timer for 2 seconds
+#	var timer = Timer.new()
+#	timer.set_wait_time(2.0)
+#	timer.timeout.connect(_on_timer_timeout)
+#	add_child(timer)
+#	timer.start()
+#
+#
+#func _on_timer_timeout() -> void:
+#	if tiles_to_return.size() > 0:
+#		_return_tile(tiles_to_return.pop_front())
 
 func _return_tile(global_pos: Vector2) -> void:
 	var cell_pos = local_to_map(global_pos)
 	var atlas_coords = get_cell_atlas_coords(0, global_pos)
 	set_cell(0, global_pos, 2, atlas_coords)
-
-func _on_timer_timeout() -> void:
-	if tiles_to_return.size() > 0:
-		_return_tile(tiles_to_return.pop_front())
