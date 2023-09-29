e4751cbf83d66a5935006eb232f50c87dfeff33f
andhedrew <theandhedrew@gmail.com>
2023-09-28 21:07:58 -0500

    pin dropping into map.

--- map_manager.gd
+++ map_manager.gd
@@ -10,17 +10,21 @@ var new_scene : String
 var move_pin := false
 var moving_paper := false
 var paper_pos := 0.0
+var drop_pin := false
+var starting_path_pos := Vector2.ZERO
 
 @onready var paper := $Control/paper
 @onready var roll := $Control/roll
 @onready var path_follow := $Control/Path2D/PathFollow2D
+@onready var path := $Control/Path2D
 
 
 func _ready():
+	starting_path_pos = path.position
 	paper_pos = - ((paper.texture.get_width()/8) * world_number)
 	paper.position.x = paper_pos
 	roll.position = Vector2(-5080, -320)
-	_setup(1.0, "res://levels_and_level_objects/level_scenes/world_1_levels/1-2.tscn")
+	_setup(5.0, "res://levels_and_level_objects/level_scenes/world_1_levels/1-2.tscn")
 
 	GameEvents.map_started.connect(_setup) #pass position, next scene
 	await get_tree().create_timer(2.0).timeout
@@ -32,8 +36,13 @@ func _process(_delta):
 		var move_speed := 0.02
 		paper.position.x = lerp(paper.position.x, paper_pos, move_speed)
 		roll.position.x = lerp(roll.position.x, roll.position.x+640, move_speed)
-		if paper.position.x == paper_pos:
-			move_pin = true
+		if paper.position.x-5 <= paper_pos:
+			drop_pin = true
+	if drop_pin:
+		var tween = create_tween()
+		tween.tween_property(path, "position", starting_path_pos, 0.2).set_trans(Tween.TRANS_LINEAR)
+		
+		#move_pin = true
 	if move_pin:
 		path_follow.progress_ratio = lerp(path_follow.progress_ratio, pos, 0.05)
 		if path_follow.progress_ratio+0.01 >= pos:
@@ -44,26 +53,33 @@ func _process(_delta):
 
 
 func _setup(new_position: float, next_scene: String) -> void:
-	if new_position == 1.0:
+	if new_position == 5.0:
+		path.position.y = -400
+		path_follow.progress_ratio = 0.0
+		pos = 0.0
 		paper_pos -= paper.texture.get_width()/8
 		await animation_player.animation_finished
+		
 
 		moving_paper = true
 		move_pin = false
 	else:
-		move_pin = true
-
-	var start_pos = new_position - 1.0
-	var end_pos = new_position/number_of_levels
-	var start_pos_adj = start_pos/number_of_levels
-	new_scene = next_scene
-
-	path_follow.progress_ratio = start_pos_adj
-	pos = end_pos
-	if pos > 1:
-		pos = 1
-	await get_tree().create_timer(4.0).timeout
+		
+		
+		var start_pos = new_position - 1.0
+		var end_pos = new_position/number_of_levels
+		var start_pos_adj = start_pos/number_of_levels
+		
+		path_follow.progress_ratio = start_pos_adj
+		pos = end_pos
+		if pos > 1:
+			pos = 1
+		await get_tree().create_timer(3.0).timeout
+#		move_pin = true
 	
+	new_scene = next_scene
+	
+
 
 
 func _end_scene() -> void:
