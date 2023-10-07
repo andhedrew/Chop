4e41168443fa6d87994f519f3ae790c726b6ba5b
andhedrew <theandhedrew@gmail.com>
2023-10-06 20:35:23 -0500

    damageable props ( for want of a better name )

--- levels_and_level_objects/question_block/DamageableProp.gd
+++ levels_and_level_objects/question_block/DamageableProp.gd
@@ -1,24 +1,10 @@
-extends StaticBody2D
-class_name ExplodingProp
+extends ExplodingProp
 
-var particle_scene = preload("res://vfx/explosive_particles.tscn")
-var slice_scene = preload("res://vfx/slice.tscn")
-@export var particle_texture: Texture
-@onready var hurtbox := $Hurtbox
-@onready var my_sprite := $Sprite2D
-@onready var collision_shape := $CollisionShape2D
 
-func _ready():
-	hurtbox.area_entered.connect(_take_damage)
-
-func _take_damage(area):
-	if area is HitBox:
-		_destroy()
+@export var chopped_sprite: Texture2D
 
 func _destroy() -> void:
-	var slice = slice_scene.instantiate()
-	get_parent().add_child(slice)
-	slice.position = position
+	GameEvents.new_vfx.emit("res://vfx/slice.tscn", global_position)
 
 	var explode = particle_scene.instantiate()
 	explode.restart()
@@ -35,15 +21,14 @@ func _destroy() -> void:
 		# Calculate the area of the RectangleShape2D
 		var area = shape.extents.x * 2 * shape.extents.y * 2
 		# Calculate the number of particles based on the area
-		var num_particles = int(area / (32 * 32) * 32)
+		var num_particles = int(area / (32 * 32) * 16)
 		# Set the amount property of the GPUParticles2D
 		explode.amount = num_particles
-		print_debug(str(num_particles))
 	# Set the emission shape to box
 	explode.process_material.emission_shape = ParticleProcessMaterial.EMISSION_SHAPE_BOX
 	# Set the box extents to match the width and height of the Sprite2D
 	var new_width = collision_shape.shape.size.x
 	var new_height = collision_shape.shape.size.y
 	explode.process_material.emission_box_extents = Vector3(new_width, new_height, 0)
-
-	queue_free()
+	$Sprite2D.texture = chopped_sprite
+	
