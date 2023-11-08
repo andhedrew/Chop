b7de5846f0e2584cf0ae6151a59d4766be5772cf
andhedrew <theandhedrew@gmail.com>
2023-11-08 12:09:31 -0600

    trampoline one-way collision and fixes

--- levels_and_level_objects/trampoline/Bouncy.gd
+++ levels_and_level_objects/trampoline/Bouncy.gd
@@ -3,11 +3,13 @@ extends StaticBody2D
 @export var speed = -300
 
 func _ready():
-	var direction = Vector2.UP.rotated(rotation)
-	constant_linear_velocity = -direction * speed
+	
+#	constant_linear_velocity = -direction * speed
 	$Area2D.body_entered.connect(_on_body_entered)
 
 
 func _on_body_entered(body) -> void:
+	var direction = Vector2.UP.rotated(rotation)
 	if body is Player or body is Enemy:
 		$AnimationPlayer.play("bounce")
+		body.velocity -= direction * speed
