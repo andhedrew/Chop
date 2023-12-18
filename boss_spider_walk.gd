b759aa7f41735018e65f3a09d198668f43d6e496
andhedrew <theandhedrew@gmail.com>
2023-12-18 16:03:27 -0600

    spider_boss_start

---
+++ boss_spider_walk.gd
@@ -0,0 +1,18 @@
+extends State
+
+var speed := 10
+
+func enter(_msg := {}) -> void:
+	pass
+
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+func update(delta: float) -> void:
+	owner.position.x += speed*delta
+
+func physics_update(_delta: float) -> void:
+	pass
+
+func exit() -> void:
+	pass
