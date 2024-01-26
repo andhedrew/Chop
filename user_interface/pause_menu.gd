3929c919db8c946bd7d0191dee6e97734589178e
andhedrew <theandhedrew@gmail.com>
2024-01-25 19:52:04 -0600

    remapping controls

--- user_interface/pause_menu.gd
+++ user_interface/pause_menu.gd
@@ -2,19 +2,29 @@ extends Control
 
 @onready var main_page := $Main
 @onready var settings_page := $Settings
+@onready var current_page := main_page
+@onready var input_menu := $InputSettings
 
 var _is_paused: bool = false
+var cutscene_running := false
 
 func _ready():
 	$Main/Resume.pressed.connect(_on_resume_pressed)
 	$Main/Quit.pressed.connect(_on_quit_pressed)
 	$Main/Settings.pressed.connect(_on_settings_pressed)
 	$Settings/Back.pressed.connect(_on_back_pressed)
+	GameEvents.cutscene_started.connect(_on_cutscene_start)
+	GameEvents.cutscene_ended.connect(_on_cutscene_end)
+	$"Settings/Remap Controls".pressed.connect(_on_remap_button_pressed)
 
 func _unhandled_input(event):
-	if event.is_action_pressed("pause"):
-		_is_paused = !_is_paused
-		set_paused(_is_paused)
+	if event.is_action_pressed("pause") and !cutscene_running:
+		if current_page == settings_page or current_page == input_menu:
+			_on_back_pressed()
+		else:
+			_on_back_pressed()
+			_is_paused = !_is_paused
+			set_paused(_is_paused)
 
 func set_paused(value: bool) -> void:
 	_is_paused = value
@@ -26,11 +36,9 @@ func _on_resume_pressed():
 	set_paused(false)
 
 func _on_settings_pressed():
-	# Hide main page
+	current_page = settings_page
 	main_page.hide()
-	# Show settings page
 	settings_page.show()
-	# Keep the paused state
 	set_paused(true)
 
 func _on_quit_pressed():
@@ -39,5 +47,25 @@ func _on_quit_pressed():
 
 
 func _on_back_pressed():
-	main_page.show()
+	if current_page == input_menu:
+		input_menu.hide()
+		settings_page.show()
+		current_page = settings_page
+	else:
+		current_page = main_page
+		main_page.show()
+		settings_page.hide()
+
+
+func _on_cutscene_start():
+	cutscene_running = true
+
+func _on_cutscene_end():
+	cutscene_running = false
+
+
+func _on_remap_button_pressed():
+	input_menu.show()
 	settings_page.hide()
+	current_page = input_menu
+	
