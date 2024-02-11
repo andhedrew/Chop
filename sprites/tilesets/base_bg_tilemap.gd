b41bc808f9cdc5d46897443e9e86a40320c874fa
andhedrew <theandhedrew@gmail.com>
2024-02-11 15:27:26 -0600

    factory

--- sprites/tilesets/base_bg_tilemap.gd
+++ sprites/tilesets/base_bg_tilemap.gd
@@ -2,8 +2,8 @@ extends TileMap
 
 #IF YOU'RE LOOKING TO CREATE TILES SWAP THIS OUT FOR TIMEMAP_MANAGER.GD!
 
-func _ready():
-	z_index = SortLayer.FOREGROUND
+#func _ready():
+#	z_index = SortLayer.FOREGROUND
 	
 
 func _deflect(_position) -> void:
